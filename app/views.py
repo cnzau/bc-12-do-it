084b5bf4bd7d8c018032c15ab55f3a60b87b4c04
cnzau <clement.nzau@gmail.com>
2016-11-22 10:44:28 +0300

    Simple Home and Defaault pages with dummy data

--- app/views.py
+++ app/views.py
@@ -1,7 +1,22 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hi there! Ready For Do IT app"
+    user = {'username': 'Nzau'}  # fake user
+    boards = [  # fake array of boards
+        { 
+            'creator': {'id': 1}, 
+            'title': 'User creation' 
+        },
+        { 
+            'creator': {'id': 1}, 
+            'title': 'User auth' 
+        }
+    ]
+    return render_template('index.html',
+                           title='My To Do',
+                           user=user,
+                           boards=boards)
