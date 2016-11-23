b6ca1842b1138706d8242de3ddf732de2cb5ad22
cnzau <clement.nzau@gmail.com>
2016-11-23 20:38:57 +0300

    Added models

---
+++ app/models.py
@@ -0,0 +1,83 @@
+from app import db
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from app import bcrypt
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True,
+                         unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    boards = db.relationship('Board', backref='creator', lazy='dynamic')
+    lists = db.relationship('List', backref='creator', lazy='dynamic')
+    cards = db.relationship('Card', backref='creator', lazy='dynamic')
+    items = db.relationship('Card', backref='creator', lazy='dynamic')
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = bcrypt.generate_password_hash(password)
+
+    # flask-login methods
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.id)
+
+
+class Board(db.Model):
+    __tablename__ = 'boards'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return '<Board %r>' % (self.id)
+
+
+class List(db.Model):
+    __tablename__ = 'lists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    board_id = db.Column(db.Integer, db.ForeignKey('boards.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return '<List %r>' % (self.id)
+
+
+class Card(db.Model):
+    __tablename__ = 'cards'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.String(64), nullable=True)
+    list_id = db.Column(db.Integer, db.ForeignKey('lists.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return '<Card %r>' % (self.id)
+
+
+class Item(db.Model):
+    __tablename__ = 'items'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    status = db.Column(db.Integer, nullable=False)
+    card_id = db.Column(db.Integer, db.ForeignKey('cards.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return '<Item %r>' % (self.name)
