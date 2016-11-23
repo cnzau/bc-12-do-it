7fbf19f4a357404dd71416c39fabf74d9d0cd8c5
cnzau <clement.nzau@gmail.com>
2016-11-23 23:44:04 +0300

    Added database configurations

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'it_is_very_secret_dont_try'
+
+# Path of our database file
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+# Store the SQLAlchemy-migrate data files
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
