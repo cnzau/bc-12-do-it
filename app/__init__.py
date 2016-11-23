a9f062fd471b0cbe761749b57ee52e218c009c5f
cnzau <clement.nzau@gmail.com>
2016-11-23 20:33:47 +0300

    Initialized application and login manager instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt, check_password_hash, generate_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import os
 
+# Create the application object
 app = Flask(__name__)
-app.config.from_object('config')
 
-from app import views
+# Create a Bcrypt instance
+bcrypt = Bcrypt(app)
+
+# Create a LoginManager instance and configure for login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+#  Development environment
+app.config.from_object('config')
