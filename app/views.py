925143322a2aa9b651ccbdfcb45f10b5c9db78b9
cnzau <clement.nzau@gmail.com>
2016-11-24 13:33:02 +0300

    Created routes for authentications

--- app/views.py
+++ app/views.py
@@ -1,22 +1,57 @@
-from flask import render_template
+from flask import render_template, flash, request, redirect, url_for, session
+from flask_oauth import OAuth
 from app import app
+from app import db
+from models import User, bcrypt
+from forms import LoginForm, SignupForm
+from flask_login import login_user, login_required, logout_user, current_user
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET'])
 def index():
-    user = {'username': 'Nzau'}  # fake user
-    boards = [  # fake array of boards
-        { 
-            'creator': {'id': 1}, 
-            'title': 'User creation' 
-        },
-        { 
-            'creator': {'id': 1}, 
-            'title': 'User auth' 
-        }
-    ]
-    return render_template('index.html',
-                           title='My To Do',
-                           user=user,
-                           boards=boards)
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    form = LoginForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            email = form.email.data
+            password = form.password.data
+
+            user = User.query.filter_by(email=email).first()
+            if user is not None and bcrypt.check_password_hash(user.password, request.form['password']):
+                login_user(user)
+                flash('Login successful!')
+                return redirect(url_for('index'))
+            else:
+                error = 'Invalid username or password. Please try again.'
+    return render_template('login.html', form=form, error=error)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You were just logged out!')
+    return redirect(url_for('login'))
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            user = User(
+                email=form.email.data,
+                username=form.username.data,
+                password=form.password.data
+            )
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('login'))
+        else:
+            flash('Invalid credentials. Please try again.')
+    return render_template('signup.html', form=form)
